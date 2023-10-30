0ec6ad91878efb54ab17d862774faab298be0317
Ivan Dolinkin <dolinkinivan@yandex.ru>
2023-10-30 13:12:36 +0300

    Task 2.1 done

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -8,10 +8,10 @@ DATA = {
         'соль, г': 0.2,
     },
     'pasta': {
-        'макароны, кг': 0.3,
-        'сыр, кг': 0.05,
+        'макароны, кг': 0.15,
+        'сыр, кг': 0.03,
     },
-    'butter': {
+    'sandwich': {
         'хлеб, ломтик': 1,
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
@@ -20,20 +20,16 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
-
 
 def recipe_view(request, recipe):
+    ingredients = {}
     servings = int(request.GET.get('servings', 1))
-    res = recipe + ':<br>'
-    for k, v in DATA[recipe].items():
-        res += k + '&#9;&#8212;&#9;' + str(v * servings) + '<br>'
+    if DATA.get(recipe):
+        for k, v in DATA.get(recipe).items():
+            ingredients[k] = v * servings
+    context = {
+        'recipe': ingredients,
+        'head': recipe
+    }
+    res = render(request, 'calculator/index.html', context)
     return HttpResponse(res)
